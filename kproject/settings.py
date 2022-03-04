b72951b73ab1ed1b761ab7f4af406c51553b9146
MindMansion <smithkelvin360@gmail.com>
2022-03-04 00:36:31 -0600

    updated setting

--- kproject/settings.py
+++ kproject/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(%ox1)+tge!a34e9628r3=-yfk(8mb)k-!)#9iy%fnwd1)3(!('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 

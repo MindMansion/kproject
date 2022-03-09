c01d14d941f13b483ebb59bb4ad20d99db05abc2
MindMansion <smithkelvin360@gmail.com>
2022-03-08 20:27:57 -0600

    updated templates

--- static/js/tippyJSFIle.js
+++
@@ -1,8 +0,0 @@
-tippy('#button', {
-  content(reference) {
-    const id = reference.getAttribute('data-template');
-    return document.getElementById(id);
-  },
-  trigger: 'click',
-  allowHTML: true,
-});
